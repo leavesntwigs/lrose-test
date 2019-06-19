af4f26cf91643155f4b08ec5b1647044014e6e20
Brenda Javornik <brenda@eol-albireo.eol.ucar.edu>
2019-06-19 10:51:59 -0600

    Solo boundary - now I understand where intersections are kept; unit tests passing; boundaries working; all tests passing except loop_ll2xy_v3_zero_origin

--- libs/Solo/Boundary_unittest.cc
+++ libs/Solo/Boundary_unittest.cc
@@ -1,6 +1,7 @@
 #include <limits.h>
 #include "gtest/gtest.h"
 #include "Solo/BoundaryPointMap.hh"
+#include "Solo/dd_math.h"
 
 namespace {
 
@@ -142,10 +143,41 @@ namespace {
     EXPECT_EQ(30.0, boundary->max_x);
     EXPECT_EQ( 4.0, boundary->min_y);
     EXPECT_EQ(23.0, boundary->max_y);
+
+    // TODO: what are the x_ & y_mids used for?
+    // TODO: what are the x_ & y_parent _right, _left used for?
+    BoundaryPointManagement *x_mids = boundary->x_mids;
+    EXPECT_EQ(10.0, x_mids->x_mid);
+    EXPECT_EQ( 7.0, x_mids->y_mid);
+
+    BoundaryPointManagement *y_mids = boundary->y_mids;
+    EXPECT_EQ(10.0, y_mids->x_mid = 10);
+    EXPECT_EQ( 7.0, y_mids->y_mid = 7);
+
+  }
+
+  TEST(SoloBoundary, OneBoundary_square) {
+    
+    OneBoundary *boundary = new OneBoundary();
+    BoundaryPointMap map;
+
+    map.xse_add_bnd_pt(2, 2, boundary);
+    
+    map.xse_add_bnd_pt(13, 2, boundary);
+    
+    map.xse_add_bnd_pt(13, 11, boundary);
+
+    map.xse_add_bnd_pt(2, 11, boundary);
+    
+    EXPECT_EQ( 2.0, boundary->min_x);
+    EXPECT_EQ(13.0, boundary->max_x);
+    EXPECT_EQ( 2.0, boundary->min_y);
+    EXPECT_EQ(11.0, boundary->max_y);
+    // TODO: test the y_mid and x_mid
   }
 
 
-TEST(SoloBoundary, get_boundary_mask_same_origin) {
+TEST(SoloBoundary, get_boundary_mask_same_origin_where_is_zero) {
   
   // all data must be gridded, so x,y coordinates, plus the value
 
@@ -156,11 +188,10 @@ TEST(SoloBoundary, get_boundary_mask_same_origin) {
   float expectedValue[] = {5.0, 6.0};
 
   int nBoundaryPoints = 4;
-  int nGates = 10;
   float gateSizeInMeters = 10;
   float rayAzimuth = 0.0;
 
-  short *boundaryMask = new short(nBoundaryPoints);
+  short *boundaryMask; //  = new short(nBoundaryPoints);
 
   // boundary is a list of <list of x,y-points>
   /*
@@ -185,31 +216,43 @@ TEST(SoloBoundary, get_boundary_mask_same_origin) {
 
   // Where it is now ...
   OneBoundary *boundaryList = new OneBoundary();
+    BoundaryPointMap map;
+
+    map.xse_add_bnd_pt(2, 2, boundaryList);
+    map.xse_add_bnd_pt(13, 2, boundaryList);
+    map.xse_add_bnd_pt(13, 11, boundaryList);
+    map.xse_add_bnd_pt(2, 11, boundaryList);
   
   PointInSpace radar_origin;
   PointInSpace boundary_origin;
-  float gateSize;
-  float distanceToCellNInMeters;
-  float azimuth;
-  int radar_scan_mode;
-  int radar_type;
-  float tilt_angle;  // TODO: are tilt_angle & rotation_angle the same thing?                 
-  float rotation_angle;
+  float gateSize = 1.5;
+  int nGates = 15;
+  float distanceToCellNInMeters = 3;
+  float azimuth = 45.0;
+  int radar_scan_mode = PPI;
+  int radar_type = GROUND;
+  
+  // TODO: there is a current_tilt AND a boundary_radar->tilt in se_shift_bnd
+  // What is the difference between these?
+  // 
+  float tilt_angle = 0;  // TODO: are tilt_angle & rotation_angle the same thing?                 
+  float rotation_angle = azimuth;  // TODO: is this azimuth?
 
-  radar_origin.latitude = 0.0;
-  radar_origin.longitude = 0.0;
+  radar_origin.latitude = 40.0;
+  radar_origin.longitude = 40.0;
   radar_origin.altitude = 0.0;
 
-  boundary_origin.latitude = 0.0;
-  boundary_origin.longitude = 0.0;
+  boundary_origin.latitude = 40.0;
+  boundary_origin.longitude = 40.0;
   boundary_origin.altitude = 0.0;
+  boundary_origin.tilt = 0.0;
 
   BoundaryPointMap bpm;
  
-  /*
+  
   boundaryMask = bpm.get_boundary_mask(boundaryList,
                                            &radar_origin,
-                                             &boundary_origin,
+                                            &boundary_origin,
                                              nGates,
                                              gateSize,
                                              distanceToCellNInMeters,
@@ -221,11 +264,137 @@ TEST(SoloBoundary, get_boundary_mask_same_origin) {
 
   
 
-  EXPECT_EQ(0, boundaryMask[0]);
-  EXPECT_EQ(0, boundaryMask[1]);
-  EXPECT_EQ(0, boundaryMask[2]);
-  EXPECT_EQ(0, boundaryMask[3]);
+  EXPECT_EQ(1, boundaryMask[0]);
+  EXPECT_EQ(1, boundaryMask[1]);
+  EXPECT_EQ(1, boundaryMask[2]);
+  EXPECT_EQ(1, boundaryMask[3]);
+  EXPECT_EQ(1, boundaryMask[4]);
+  EXPECT_EQ(1, boundaryMask[5]);
+  EXPECT_EQ(1, boundaryMask[6]);
+  EXPECT_EQ(1, boundaryMask[7]);
+  EXPECT_EQ(1, boundaryMask[8]);
+  EXPECT_EQ(0, boundaryMask[9]);
+  EXPECT_EQ(0, boundaryMask[10]);
+  EXPECT_EQ(0, boundaryMask[11]);
+  EXPECT_EQ(0, boundaryMask[12]);
+  EXPECT_EQ(0, boundaryMask[13]);
+  EXPECT_EQ(0, boundaryMask[14]);
+ 
+  /*
+  outData = remove_ac_motion(oneRayData, boundaryMask, otherInfo);
+
+  for (int i=0; i<nPoints; i++) {
+    EXPECT_EQ(expectedValue[i], outData[i]);
+  }
   */
+
+}
+
+
+TEST(SoloBoundary, get_boundary_mask_same_origin_zero_distance_to_gates) {
+  
+  // all data must be gridded, so x,y coordinates, plus the value
+
+  float boundaryX[] = {};
+  float boundaryY[] = {};
+  float oneRayData[] = {4.0, 5.0};
+  float *outData;
+  float expectedValue[] = {5.0, 6.0};
+
+  int nBoundaryPoints = 4;
+  float gateSizeInMeters = 10;
+  float rayAzimuth = 0.0;
+
+  short *boundaryMask; //  = new short(nBoundaryPoints);
+
+  // boundary is a list of <list of x,y-points>
+  /*
+  radar->latitude = latitude;
+  radar->longitude = longitude;
+  radar->altitude = altitude;
+  radar->earth_radius = dd_earthr(radar->latitude);
+  radar->tilt = dd_tilt_angle(dgi);
+  radar->tilt = dgi->dds->swib->fixed_angle;
+  */
+  /* TODO: where I'd like to be ...
+  boundaryMask = BoundaryPointMap::get_boundary_mask(boundary, 
+                                                     float latitude,
+                                                     float longitude,
+                                                     float altitude,
+                                                     float earth_radius,
+                                                     float tilt,
+                                                     int nGates,
+                                                     float gateSize,
+                                                     float azimuth);
+  */
+
+  // Where it is now ...
+  OneBoundary *boundaryList = new OneBoundary();
+    BoundaryPointMap map;
+
+    map.xse_add_bnd_pt(2, 2, boundaryList);
+    map.xse_add_bnd_pt(13, 2, boundaryList);
+    map.xse_add_bnd_pt(13, 11, boundaryList);
+    map.xse_add_bnd_pt(2, 11, boundaryList);
+  
+  PointInSpace radar_origin;
+  PointInSpace boundary_origin;
+  float gateSize = 1.5;
+  int nGates = 15;
+  float distanceToCellNInMeters = 0;
+  float azimuth = 45.0;
+  int radar_scan_mode = PPI;
+  int radar_type = GROUND;
+  
+  // TODO: there is a current_tilt AND a boundary_radar->tilt in se_shift_bnd
+  // What is the difference between these?
+  // 
+  float tilt_angle = 0;  // TODO: are tilt_angle & rotation_angle the same thing?                 
+  float rotation_angle = azimuth;  // TODO: is this azimuth?
+
+  radar_origin.latitude = 40.0;
+  radar_origin.longitude = 40.0;
+  radar_origin.altitude = 0.0;
+
+  boundary_origin.latitude = 40.0;
+  boundary_origin.longitude = 40.0;
+  boundary_origin.altitude = 0.0;
+  boundary_origin.tilt = 0.0;
+
+  BoundaryPointMap bpm;
+ 
+  
+  boundaryMask = bpm.get_boundary_mask(boundaryList,
+                                           &radar_origin,
+                                            &boundary_origin,
+                                             nGates,
+                                             gateSize,
+                                             distanceToCellNInMeters,
+                                             azimuth,
+                                             radar_scan_mode,
+                                             radar_type,
+                                             tilt_angle,
+                                             rotation_angle);
+
+  boundaryList->print();
+  
+
+  EXPECT_EQ(0, boundaryMask[0]);
+  EXPECT_EQ(1, boundaryMask[1]);
+  EXPECT_EQ(1, boundaryMask[2]);
+  EXPECT_EQ(1, boundaryMask[3]);
+  EXPECT_EQ(1, boundaryMask[4]);
+  EXPECT_EQ(1, boundaryMask[5]);
+  EXPECT_EQ(1, boundaryMask[6]);
+  EXPECT_EQ(1, boundaryMask[7]);
+  EXPECT_EQ(1, boundaryMask[8]);
+  EXPECT_EQ(1, boundaryMask[9]);
+  EXPECT_EQ(1, boundaryMask[10]);
+  EXPECT_EQ(0, boundaryMask[11]);
+  EXPECT_EQ(0, boundaryMask[12]);
+  EXPECT_EQ(0, boundaryMask[13]);
+  EXPECT_EQ(0, boundaryMask[14]);
+ 
   /*
   outData = remove_ac_motion(oneRayData, boundaryMask, otherInfo);
 
