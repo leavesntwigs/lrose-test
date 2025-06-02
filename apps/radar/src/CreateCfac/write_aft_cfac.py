c3faad3a755118fc0cce7552c0201807ce511b8f
leavesntwigs <bjav7496@gmail.com>
2025-06-02 10:14:07 -0600

    cfac.aft agreement with python & fortran versions

--- apps/radar/src/CreateCfac/write_aft_cfac.py
+++ apps/radar/src/CreateCfac/write_aft_cfac.py
@@ -1,5 +1,7 @@
 # Write the aft cafc file
 
+import os.path
+
 def write_aft_cfac(directory, range_delay_corr_aft,
     pressure_alt_corr, ew_gndspd_corr, 
     pitch_corr_cfac, drift_corr_cfac,
@@ -10,25 +12,25 @@ def write_aft_cfac(directory, range_delay_corr_aft,
         # open(11,file=directory(1:ndir)//'/'//'cfac.aft'
         #     ,form='formatted',status='unknown')
 
-        f11.write(f'{"azimuth_corr           ="}{0.0:8.3f}')
+        f11.write(f'{"azimuth_corr           ="}{0.0:8.3f}\n')
 
-        f11.write(f'{"elevation_corr         ="}{0.0:8.3f}')
+        f11.write(f'{"elevation_corr         ="}{0.0:8.3f}\n')
 
-        f11.write(f'{"range_delay_corr       ="}{range_delay_corr_aft:8.3f}')
+        f11.write(f'{"range_delay_corr       ="}{range_delay_corr_aft:8.3f}\n')
 
-        f11.write(f'{"longitude_corr         ="}{0.0:8.3f}')
-        f11.write(f'{"latitude_corr          ="}{0.0:8.3f}')
-        f11.write(f'{"pressure_alt_corr      ="}{pressure_alt_corr:8.3f}')
-        f11.write(f'{"radar_alt_corr         ="}{0.0:8.3f}')
-        f11.write(f'{"ew_gndspd_corr         ="}{ew_gndspd_corr:8.3f}')
+        f11.write(f'{"longitude_corr         ="}{0.0:8.3f}\n')
+        f11.write(f'{"latitude_corr          ="}{0.0:8.3f}\n')
+        f11.write(f'{"pressure_alt_corr      ="}{pressure_alt_corr:8.3f}\n')
+        f11.write(f'{"radar_alt_corr         ="}{0.0:8.3f}\n')
+        f11.write(f'{"ew_gndspd_corr         ="}{ew_gndspd_corr:8.3f}\n')
 
-        f11.write(f'{"ns_gndspd_corr         ="}{0.0:8.3f}')
-        f11.write(f'{"vert_vel_corr          ="}{0.0:8.3f}')
-        f11.write(f'{"heading_corr           ="}{0.0:8.3f}')
-        f11.write(f'{"roll_corr              ="}{0.0:8.3f}')
-        f11.write(f'{"pitch_corr             ="}{pitch_corr_cfac:8.3f}')
-        f11.write(f'{"drift_corr             ="}{drift_corr_cfac:8.3f}')
-        f11.write(f'{"rot_angle_corr         ="}{rot_angle_corr_aft:8.3f}')
-        f11.write(f'{"tilt_corr              ="}{tilt_corr_aft:8.3f}')
+        f11.write(f'{"ns_gndspd_corr         ="}{0.0:8.3f}\n')
+        f11.write(f'{"vert_vel_corr          ="}{0.0:8.3f}\n')
+        f11.write(f'{"heading_corr           ="}{0.0:8.3f}\n')
+        f11.write(f'{"roll_corr              ="}{0.0:8.3f}\n')
+        f11.write(f'{"pitch_corr             ="}{pitch_corr_cfac:8.3f}\n')
+        f11.write(f'{"drift_corr             ="}{drift_corr_cfac:8.3f}\n')
+        f11.write(f'{"rot_angle_corr         ="}{rot_angle_corr_aft:8.3f}\n')
+        f11.write(f'{"tilt_corr              ="}{tilt_corr_aft:8.3f}\n')
 
 
